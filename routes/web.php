899a35cfd725c4a65ae9db5974de060aa65e5d9f
Mildred Manrique <mildred.manrique@outlook.es>
2022-03-22 17:24:26 -0600

    The views of the general post and the single post were created

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@posts');
+Route::get('blog/{post}', 'PageController@post')->name('post');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
