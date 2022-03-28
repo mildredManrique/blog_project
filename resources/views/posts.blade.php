7af7d8432ed1202b35d66c5f08f12fa66e0749fd
Mildred Manrique <mildred.manrique@outlook.es>
2022-03-28 10:42:58 -0600

    It was added in the Article view to be able to add image and video in a single post.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,15 +6,15 @@
         <div class="col-md-8">
             @foreach ($posts as $post )
             <div class="card mb-4">
+                @if ($post->image)
+                <img src="{{ $post->get_image }}" class="card-img-top">
+                @elseif ($post->iframe)
+                <div class="embed-responsive embed-responsive-16by9">
+                    {!! $post->iframe !!}
+                </div>
+                @endif
                 <div class="card-body">
-                    @if ($post->image)
-                        <img src="{{ $post->get_image }}" class="card-img-top">
-                    @elseif ($post->iframe)
-                        <div class="embed-responsive embed-responsive-16by9">
-                        {!! $post->iframe !!}
-                        </div>
-                    @endif
-                    <h5 class ="card-title">{{ $post->title }}</h5>
+                    <h5 class="card-title">{{ $post->title }}</h5>
                     <p class="card-text">
                         {{ $post->get_excerpt }}
                         <a href="{{ route('post', $post) }}">Leer más...</a>
